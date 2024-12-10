616bc159c37a857a1e61b6fa8e51894b887de6b4
Bryant Mejia <bmejia220@gmail.com>
2024-12-10 09:55:27 -0500

    getprerenderparams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'analytics/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams:() => {
+      return new Promise(resolve => resolve([1,2,3,4,5].map(id => ({ id: id.toString() }))))
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
